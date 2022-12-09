bd02da90edde06a6f270f5f3ebba24d6a0b36347
Peter La Follette <peterlafollette@Peters-MacBook-Pro-2.local>
2022-12-08 16:30:26 -0800

    cleaning up parameter files

--- parameter_files/params_Phillipsburg.py
+++ parameter_files/params_Phillipsburg.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-#here, layer parameters are defined.
+###In the first block, layer parameters are defined. The second block does not need to be adjusted by the user.
 
+
+
+
+#######
+
+#here, the parameter h_p_max, or the maximum ponding depth (in mm), is defined.
+h_p_max = 0 
+wilting_point_head = 154950 #this is the pressure head in mm (but negative) that represents the pressure of the wilting point. A value of 154950 mm physically indicates, in LGAR, a wilting point of -15 atmospheres. This is used to calculate the volumetric water content which represents the wilting point.
+relative_moisture_at_which_PET_equals_AET = 0.75 #relative moisture at which AET is approximately the same as PET
 num_layers = 3
 
 #first layer: loam (HYDRUS catalog)
@@ -30,7 +39,6 @@ max_depth_layer_1       = 1310    #mm
 param_set_1 = [theta_r_layer_1, theta_s_layer_1, K_s_layer_1, alpha_layer_1, n_layer_1, m_layer_1, max_depth_layer_1]
 
 
-
 #second layer: silty clay loam (finer than layer above) (HYDRUS catalog)
 theta_r_layer_2         = 0.0668  #residual volumetric water content.            unitless
 theta_s_layer_2         = 0.4617   #total porosity.                               unitless
@@ -43,6 +51,15 @@ max_depth_layer_2       = 250 #10000   #mm
 param_set_2 = [theta_r_layer_2, theta_s_layer_2, K_s_layer_2, alpha_layer_2, n_layer_2, m_layer_2, max_depth_layer_2]
 
 
+#######
+
+
+
+
+
+
+#######
+
 #also making vectors of parameters based on parameter name
 theta_r_vec   = [theta_r_layer_0,   theta_r_layer_1,   theta_r_layer_2]
 theta_s_vec   = [theta_s_layer_0,   theta_s_layer_1,   theta_s_layer_2]
@@ -51,28 +68,6 @@ alpha_vec     = [alpha_layer_0,     alpha_layer_1,     alpha_layer_2]
 n_vec         = [n_layer_0,         n_layer_1,         n_layer_2]
 m_vec         = [m_layer_0,         m_layer_1,         m_layer_2]
 max_depth_vec = [max_depth_layer_0, max_depth_layer_1, max_depth_layer_2]
-
-#here, the parameter h_p_max, or the maximum ponding depth (in mm), is defined. It is defined separately because it is the only parameter that is independent of individual layers.
-h_p_max = 0 #20
-
-wilting_point_head = 154950 #this is the pressure head in mm (but negative) that represents the pressure of the wilting point. A value of 154950 mm physically indicates, in LGAR, a wilting point of -15 atmospheres. This is used to calculate the volumetric water content which represents the wilting point.
-relative_moisture_at_which_PET_equals_AET = 0.75 #0.75 #technically as currently implemented this is just the factor by which theta_s is multiplied
-
-
-
-
-
-###
-###for testing what happens when 2 adjacent layers are the same
-# theta_r_vec   = [theta_r_layer_0,   theta_r_layer_0,   theta_r_layer_2]
-# theta_s_vec   = [theta_s_layer_0,   theta_s_layer_0,   theta_s_layer_2]
-# K_s_vec       = [K_s_layer_0,       K_s_layer_0,       K_s_layer_2]
-# alpha_vec     = [alpha_layer_0,     alpha_layer_0,     alpha_layer_2]
-# n_vec         = [n_layer_0,         n_layer_0,         n_layer_2]
-# m_vec         = [m_layer_0,         m_layer_0,         m_layer_2]
-# max_depth_vec = [max_depth_layer_0, max_depth_layer_0, max_depth_layer_2]
-# param_set_1 = param_set_0
-###
 
 #also putting parameter sets into array because it is necessary for loop that considers all wetting fronts in all layers
 parameters = np.vstack([param_set_0,param_set_1,param_set_2])
@@ -87,3 +82,5 @@ temp_bdy_depth = 0
 for s in range(0,len(parameters)):
     temp_bdy_depth = temp_bdy_depth + parameters[s][-1]
     boundary_depths.append(temp_bdy_depth)
+
+#######
